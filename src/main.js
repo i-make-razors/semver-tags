e4ede640aa4f794ba09b03464bbed56ac8c6fedb
Colin Dickerson <dickerson.cg@pg.com>
2024-12-19 16:04:11 -0500

    Build pre-release

--- src/main.js
+++ src/main.js
@@ -18,7 +18,9 @@ function generateVersionPattern(options) {
 async function calculateNextVersion(previous) {
   const defaultVersion = core.getInput('default-version');
   const incrementedValue = core.getInput('incremented-value');
-  const prerelease = core.getInput('prerelease');
+  const ref = core.getInput('ref');
+  const commitSha = core.getInput('commit-sha');
+  const defaultRef = core.getInput('default-ref');
   const tagPrefix = core.getInput('tag-prefix');
   const versionPattern = generateVersionPattern({ tagPrefix: tagPrefix });
 
@@ -73,9 +75,12 @@ async function calculateNextVersion(previous) {
   console.log(`Core version: ${semanticVersion}`);
   core.setOutput('core-version', semanticVersion);
 
-  if(prerelease) {
-    console.log(`Prerelease configured. Adding '-${ prerelease }' to version number.`);
-    semanticVersion += `-${prerelease}`;
+  if (ref != defaultRef) {
+    //not on default branch
+    let shortSha = commitSha.substring(0, 6)
+    let prerelease = `-pr.${shortSha}`
+    console.log(`Not on main branch. Adding '${ prerelease }' to version number.`);
+    semanticVersion += `${prerelease}`;
   }
 
   console.log(`Semantic version: ${semanticVersion}`);
